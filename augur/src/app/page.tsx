50c8eb089fe30908b676d9c84c6e0f2876093044
Scott Zembsch <scottzembsch@gmail.com>
2024-05-30 19:55:27 -0700

    working tailwind, and link to signup page

--- augur/src/app/page.tsx
+++ augur/src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <h1>Home page</h1>
+import Link from "next/link"
+
+
+function Home() {
+  return <>
+    <h1 className="text-4xl font-semibold">Welcome to Augur</h1>
+    <h2 className="text-xl font-extralight">A Place For Devs & Their Thoughts</h2>
+    <Link className="text-green-400" href="/signup">Signup</Link>
+  </> 
+  
 }
+
+export default Home;

9a5d8b2290e5b65b9dd59f832c78357db4955b7a
Andranik Agamalian <128445370+letscode94@users.noreply.github.com>
2024-06-03 23:01:27 -0700

    added Quill Editor that converts text to markdown

--- augur/src/app/post/page.tsx
+++ augur/src/app/post/page.tsx
@@ -1,12 +1,31 @@
-import React from 'react'
+'use client';
+import { useState } from "react";
+import Editor, { EditorContentChanged } from "../Editor";
 
+import dynamic from 'next/dynamic';
 
-function post() {
+const DynamicEditor = dynamic(() => import('../Editor'), { ssr: false });
+
+// const initialMarkdownContent = "**StartInitial** writing *something*...";
+
+export default function Post() {
+  const [editorMarkdownValue, setEditorMarkdownValue] = useState<string>("");
+
+  const onEditorContentChanged = (content: EditorContentChanged) => {
+    setEditorMarkdownValue(content.markdown);
+  };
+
+  const handleClick = () => {
+    console.log('markdown', editorMarkdownValue)
+  }
+
   return (
-  <>
-    <div>post</div>
-  </>
-  )
+    <div className="border-2 border-red-500 m-10">
+      <DynamicEditor
+        // value={initialMarkdownContent}
+        onChange={onEditorContentChanged}
+      /> 
+      <button onClick={handleClick} className="border-2 border-red-500 p-1 m-1 rounded ">Submit Blog</button>
+    </div>
+  );
 }
-
-export default post

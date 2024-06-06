3b9331f99efd88f57438b46445eced61d83e2cac
George Huang <gzh@alumni.princeton.edu>
2024-06-05 19:06:58 -0700

    landing page completed with UI/UX design

--- augur/src/app/page.tsx
+++ augur/src/app/page.tsx
@@ -1,75 +1,22 @@
 'use client';
-
 import Link from "next/link"
 import React from "react";
-import { useState } from "react";
-
-
 import { useRouter } from "next/navigation";
-
+import Nav from "@/components/Nav";
+import Hero from "@/components/Hero";
+import Footer from "@/components/Footer";
+import MultiSection from "@/components/MultiSection";
 
 
 function Home() {
-
-  const [loginData, setLoginData] = useState({username : '', password : ''});
-  const router = useRouter();
-
-  interface LoginData {
-    username: string;
-    password: string;
-  }
-
-  interface ApiResponse {
-    message: string;
-  }
-
-  const submitLogin = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const username = formData.get('username') as string;
-    const password = formData.get('password') as string;
-    const loginData = { username: username, password: password };
-    response(loginData);
-  };
-  
-  
-    // Send data to the API route for validation
-    const response = async (data: LoginData) => {
-      try {
-        const res = await fetch('/api/login', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(data),
-        });
-  
-        const result: ApiResponse = await res.json(); 
-        if (!result.message) {
-          alert('Invalid login');
-          return;
-        } else {
-          router.push('/feed');
-        }
-
-      } catch (error) {
-        console.error('An error occurred:', error);
-      }
-    };
-
-
-  return <>
-    <h1 className="text-4xl font-semibold">Welcome to Blaugur</h1>
-    <h2 className="text-xl font-extralight">A Place For Devs & Their Thoughts</h2>
-    <Link className="text-green-400" href="/signup">Signup</Link>
-
-    <form onSubmit={submitLogin}>
-      <input type="text" name="username" id="" placeholder="username" />
-      <input type="text" name="password" placeholder="Password"/>
-      <input type="submit" />
-    </form>
-  </> 
-  
+  return (
+    <>
+      <Nav/>
+      <Hero/>
+      <MultiSection/>
+      <Footer/>
+    </>
+  )
 }
 
 export default Home;

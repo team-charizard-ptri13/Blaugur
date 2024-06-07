7de2a3e73cc69a023c00ad511cb671ddc37ab510
Scott Zembsch <scottzembsch@gmail.com>
2024-06-06 20:43:05 -0700

    added default user_pic when users signup

--- augur/src/app/api/image/route.ts
+++ augur/src/app/api/image/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 import { S3Client, PutObjectCommand, GetObjectCommand, ObjectCannedACL } from '@aws-sdk/client-s3';
 import dotenv from 'dotenv';
 import crypto from 'crypto';
-import Blog from '../../Model/Blog';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
 
 
@@ -84,16 +84,40 @@ export async function POST(request:any){
 }
 
 
-export async function GET(request:any){
+// export async function GET(request:any){
+//   try {
+//     const posts = await Blog.find({})
+//     console.log('anything here?',posts)
+//     return new Response(JSON.stringify(posts));
+//   } catch (err){
+//     return new Response(JSON.stringify({ message: 'File uploaded failed' }));;
+//   }
+// }
+
+export async function GET(request:any) {
   try {
-    const posts = await Blog.find({})
-    console.log('anything here?',posts)
-    return new Response(JSON.stringify(posts));
-  } catch (err){
-    return new Response(JSON.stringify({ message: 'File uploaded failed' }));;
+    
+    const defaultImage = 'default_user_color.png'
+    
+    // Generate signed URLs for each blog's image
+
+      const getObjectParams = {
+        Bucket: bucketName,
+        Key: defaultImage,
+      };
+      
+      const command = new GetObjectCommand(getObjectParams);
+      const url = await getSignedUrl(s3, command, { expiresIn: 9999 });
+      
+      // Add the signed URL to the blog object
+      // return { defaultImage_url: url };
+
+    return NextResponse.json({ message: 'All Blogs fetched successfully', defaultImage: url });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: 'Error fetching all blogs' }, { status: 500 });
   }
 }
-
 
 
 
